f611032d886eb93b95a45a91c07391768669b28d
myphproad <857773627@qq.com>
2014-03-29 20:01:45 +0800

    link

--- Admin/Lib/Action/LinkAction.class.php
+++ Admin/Lib/Action/LinkAction.class.php
@@ -1,4 +1,61 @@
 <?php
-// 链接模块
+/*
+ * name:友情链接
+ * aothor:caoding 
+ */
 class LinkAction extends CommonAction {
+	public function index() {
+		//首页
+		$M = M("Link");
+		$count = $M->count();
+		import("ORG.Util.Page");       //载入分页类
+		$page = new Page($count, 10);
+		$showPage = $page->show();
+		$this->assign("page", $showPage);
+		$this->assign("list", D("Link")->listNews($page->firstRow, $page->listRows));
+		$this->display();
+	}
+
+	
+	public function add() {
+		//增加
+		if (IS_POST) {
+			$this->checkToken();
+			echo json_encode(D("Article")->addArticle());
+		} else {
+			$this->assign("list", D("Article")->category());
+			$this->display();
+		}
+	}
+	
+
+	
+	public function edit() {
+		//编辑
+		$M = M("Article");
+		if (IS_POST) {
+			$this->checkToken();
+			echo json_encode(D("Article")->edit());
+		} else {
+			$info = $M->where("id=" . (int) $_GET['id'])->find();
+			if ($info['id'] == '') {
+				$this->error("不存在该记录");
+			}
+			$this->assign("info", $info);
+			$this->assign("list", D("Article")->category());
+			$this->display("add");
+		}
+	}
+	
+	public function del() {
+		//删除
+		if (M("Article")->where("id=" . (int) $_GET['id'])->delete()) {
+			$this->success("成功删除");
+			//            echo json_encode(array("status"=>1,"info"=>""));
+		} else {
+			$this->error("删除失败，可能是不存在该ID的记录");
+		}
+	}
+	
+	
 }
