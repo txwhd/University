b18e987a3f5fd13fe3186311db283840d0bda83d
txwhd <2271313916@qq.com>
2014-04-13 17:38:06 +0800

    advertisment

--- Admin/Lib/Action/advertisementAction.class.php
+++ Admin/Lib/Action/advertisementAction.class.php
@@ -18,6 +18,30 @@ class AdvertisementAction extends CommonAction {
 		//删除,调用永久删除的方法
 	
 	} */
+	public function forbidden(){
+		//禁止会员状态
+		$where['ad_id']=$_GET['id'];
+		$model=M("Advertisement");
+		$data['available'] = '2';
+		$result=$model->where($where)->save($data);
+		if($result){
+			$this->success('已经禁用该广告！');
+		}else{
+			$this->error("禁用失败！");
+		}
+	}
+	public function checkPass(){
+		//通过会员审核
+		$where['ad_id']=$_GET['id'];
+		$model=M("Advertisement");
+		$data['available'] = '1';
+		$result=$model->where($where)->save($data);
+		if($result){
+			$this->success('已经开启该广告！');
+		}else{
+			$this->error("开启失败！");
+		}
+	}
 	
 	
 }
