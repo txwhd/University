8ced99366c5a83a26c4af4d932e01a3c93362eea
myphproad <857773627@qq.com>
2014-04-19 22:21:53 +0800

    all page

--- Admin/Lib/Model/MemberModel.class.php
+++ Admin/Lib/Model/MemberModel.class.php
@@ -8,8 +8,8 @@ class MemberModel extends RelationModel{
 		public function listNews($firstRow = 0, $listRows = 20) {
 		        $M = D("Member");
 		        $list = $M->relation(true)->limit("$firstRow , $listRows")->select();
-		        print_r($list);
-		        exit();
+		       /*  print_r($list);
+		        exit(); */
 		        return $list;
 		  }
 		  /*
@@ -35,15 +35,15 @@ class MemberModel extends RelationModel{
 		  				'mapping_name'=> 'member_detail',
 		  				'class_name' => 'member_detail',
 		  				'foreign_key' => 'member_id',
-		  				'as_fields'=>'id,vip_type_id,english_name,realName,nationName'
+		  				'as_fields'=>'id,english_name,realName,nationName,schoolName,academy,academic,if_overseas,if_belif_object,belif_state,belif_name,if_belief,gender,nationality,province,city,birthday,age,height,star,blood,UserFan,drinking,smoke,job,income,house,marry,monologue,qq,msn,look_grade,goal,love_time,graduate_time,in_school,iPoint,ePoint,cPoint,aPoint,loginCount,OnlineTF,FriendClass,LastIP,isOpen,isIDcard,IdcardFiles,Addfriendbs,Emailcode,Ismobile,BindTF,attention_id,avatar,avatarwidth,avatarheight,sightml,groupterms,authstr,ifNation,limit_reply,limit_attention,limit_say,constellation'
 		  		),
-		  		'marriage_term'=>array(
+		  		/* 'marriage_term'=>array(
 		  				'mapping_type'=> HAS_ONE,
 		  				'mapping_name'=> 'marriage_term',
 		  				'class_name' => 'marriage_term',
 		  				'foreign_key' => 'member_id',
-		  				'as_fields'=>'id,member_id,vip_type_id,english_name,realName,nationName,schoolName,academy,academic,if_overseas,if_belif_object,belif_state,belif_name,if_,belief,gender,nationality,province,city,birthday,age,height,star,blood,UserFan,drinking,smoke,job,income,house,marry,monologue,qq,msn,look_grade,goal,love_time,graduate_time,in_school,iPoint,ePoint,cPoint,aPoint,loginCount,OnlineTF,FriendClass,LastIP,isOpen,isIDcard,IdcardFiles,Addfriendbs,Emailcode,Ismobile,BindTF,attention_id,avatar,avatarwidth,avatarheight,sightml,groupterms,authstr,ifNation,limit_reply,limit_attention,limit_say,constellation'
-		  		),
+		  				'as_fields'=>',english_name,realName,nationName'
+		  		), */
 		  		'viptype'=>array(
 		  				'mapping_type'=> BELONGS_TO,
 		  				'mapping_name'=> 'viptype',
@@ -51,15 +51,5 @@ class MemberModel extends RelationModel{
 		  				'foreign_key' => 'vip_type_id',
 		  				'as_fields'=>'vip_name'
 		  		),
-		  		/*  
-		  		 * 
-		  		 * mapping_type
-class_name
-mapping_name
-foreign_key
-condition
-mapping_fields
-as_fields*/
 		  );
-
 }
