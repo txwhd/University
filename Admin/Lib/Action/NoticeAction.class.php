628a5e98b0a791ae2978cf7c88d723be7526aaf1
myphproad <857773627@qq.com>
2014-04-20 15:08:24 +0800

    notice

--- Admin/Lib/Action/NoticeAction.class.php
+++ Admin/Lib/Action/NoticeAction.class.php
@@ -11,15 +11,9 @@ class NoticeAction extends CommonAction {
 	}
 	public function edit(){
 		//编辑页面
-		$M = M("Notice");
-		$where['notice_id']=$_GET['id'];
-		$list=$M->where($where)->select();
-		$this->assign("vo", $list);
-		$this->display("add");
+		
+		
 	}
 	public function _before_add(){
-		//得到ip
-		$ip = get_client_ip();
-		
 	}
 }
