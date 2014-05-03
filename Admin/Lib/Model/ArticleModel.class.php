52fb43b579cd922ded62e27d11ca059d66f9b78f
kingzyk <670362273@qq.com>
2014-05-03 16:46:04 +0800

    zyk

--- Admin/Lib/Model/ArticleModel.class.php
+++ Admin/Lib/Model/ArticleModel.class.php
@@ -1,8 +1,5 @@
 <?php
 // 文章模型
 class ArticleModel extends CommonModel {
-	public function listNews($firstRow = 0, $listRows = 20) {
-		$list =  M()->table("mxczhyk_article r")->join("mxczhyk_article_class s on s.cid=r.cid")->select();
-		return $list;
-	}
+	
 }
