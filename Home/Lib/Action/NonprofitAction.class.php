f09e206df4feb520e8c12a7fd58e31754277af75
myphproad <857773627@qq.com>
2014-05-24 11:59:00 +0800

    commfoot

--- Home/Lib/Action/NonprofitAction.class.php
+++ Home/Lib/Action/NonprofitAction.class.php
@@ -1,7 +1,5 @@
 <?php
-/*
- * 公益操作
- */
+
 class NonprofitAction extends CommonAction{
 	
 }
--- Home/Lib/Action/SecurityAction.class.php
+++ Home/Lib/Action/SecurityAction.class.php
@@ -1,12 +1,5 @@
 <?php
-/*
- * 权利保护********************right
- * 隐私声明********************privacy
- * 服务条款********************terms
- */
+
 class SecurityAction extends CommonAction{
-	public function terms(){
-		
-		
-	}
+	
 }
